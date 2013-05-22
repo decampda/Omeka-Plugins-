f535bfcb456bf6b954110ae63671f097dfc4a0fc
Dave DeCamp <decampda@gmail.com>
2013-05-22 17:28:34 -0300

    Update and rename UrlToLink.php to UrlToLinkPlugin.php
    automatically change URL's in Website contributions to hyperlinks

--- UrlToLinkPlugin.php
+++ UrlToLinkPlugin.php
@@ -11,6 +11,3 @@ class UrlToLinkPlugin extends Omeka_Plugin_AbstractPlugin
     }
 }
 
-//This plugin changes website URLs to Links. 
-//It appears you must have http:// for the plugin to recognize it needs to be changed
-//Other than that, seems to work very well. 
